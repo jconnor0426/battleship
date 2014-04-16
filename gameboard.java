b512c01efbd60db50f69c4f54329218b576b70ce
Alexander Damiani <alexdmail15@yahoo.com>
2014-04-16 10:58:07 -0400

    Updated gameboard and homepage

--- gameboard.java
+++ gameboard.java
@@ -7,8 +7,13 @@ import java.awt.Graphics;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Dimension;
+import java.awt.event.*;
+
 
 public class gameboard extends JFrame{
+	JButton[][] button_array1 = new JButton[10][10];
+	JButton[][] button_array2 = new JButton[10][10];
+	String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 	JPanel panelEast = new JPanel(new BorderLayout());
 	JPanel panelWest = new JPanel(new BorderLayout());
 	JPanel mainPanel = new JPanel(new BorderLayout());
@@ -18,50 +23,94 @@ public class gameboard extends JFrame{
 		g1.createFrame();
 	}
 
+	// Sets up frame and adds panels
 	public void createFrame(){
 		panelEast.setLayout(new FlowLayout());
 		panelWest.setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500,500);
-		PlayerBoard board1 = new PlayerBoard();
-		PlayerBoard board2 = new PlayerBoard();
+		BoardOne board1 = new BoardOne();
+		BoardTwo board2 = new BoardTwo();
 		panelEast.add(board1);
 		panelWest.add(board2);
-		mainPanel.add(panelWest, BorderLayout.WEST);
 		mainPanel.add(panelEast, BorderLayout.EAST);
+		mainPanel.add(panelWest, BorderLayout.WEST);
 		add(mainPanel);
 		setVisible(true);
 		setResizable(false);
 		repaint();
 	}
-}
 
+	// Stores button and x,y coordinates
+	class MyButton extends JButton{
+		private int row, column;
+		private JButton button;
 
-class PlayerBoard extends JPanel{
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		setPreferredSize(new Dimension(200,200));
-		GridLayout glayout = new GridLayout(10,10);
-		setLayout(glayout);
+		public MyButton(JButton bn, int x, int y){
+			button = bn;
+			column = x;
+			row = y;
+		}
 
-		for (int i = 1; i < 101; i++){
-			add(new JButton());
+		public int getRow(){
+			return row;
+		}
+
+		public int getColumn(){
+			return column;
+		}
+
+		public JButton getButton(){
+			return button;
+		}
+	}
+
+	// Left Board
+	class BoardOne extends JPanel implements ActionListener{
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			setPreferredSize(new Dimension(200,200));
+			GridLayout glayout = new GridLayout(10,10);
+			setLayout(glayout);
+
+			for (int i = 0; i < 10; i++){
+				for (int j = 0; j < 10; j++){
+					// New instance of MyButton
+					button_array1[i][j] = new MyButton(new JButton(), i, j);
+					button_array1[i][j].addActionListener(this);
+					add(button_array1[i][j]);
+				}
+			}
+		}
+
+		public void actionPerformed(ActionEvent e){
+			MyButton button = (MyButton) e.getSource();
+			System.out.println(button.getColumn() + ":" + letters[button.getRow()]);
+		}
+	}
+
+	// Right board
+	class BoardTwo extends JPanel implements ActionListener{
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			setPreferredSize(new Dimension(200,200));
+			GridLayout glayout = new GridLayout(10,10);
+			setLayout(glayout);
+
+			for (int i = 0; i < 10; i++){
+				for (int j = 0; j < 10; j++){
+					// New instance of MyButton
+					button_array2[i][j] = new MyButton(new JButton(), i, j);
+					button_array2[i][j].addActionListener(this);
+					add(button_array2[i][j]);
+				}
+			}
+		}
+
+		public void actionPerformed(ActionEvent e){
+			MyButton button = (MyButton) e.getSource();
+			System.out.println(button.getColumn() + ":" + letters[button.getRow()]);
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- homepage.java
+++ homepage.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 import java.util.Vector;
 import java.awt.GridLayout;
@@ -12,13 +13,19 @@ import javax.swing.ImageIcon;
 import javax.imageio.ImageIO;
 import java.net.URL;
 import java.awt.Image;
-
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.*;
+import java.awt.Font;
+import java.awt.Color;
 
 public class homepage extends JFrame{
-	JButton b1;
-	JLabel l1;	
+	JButton b1, b2, b3, b4;
 	URL url;
-	JLabel picture;
+	JPanel panel = new JPanel();
+	JLabel title = new JLabel("BattleShip", JLabel.CENTER);
+	Font font = new Font("Verdana", Font.BOLD, 40);
+
 	public static void main(String[] args){
 		homepage page = new homepage();
 		page.create();
@@ -26,31 +33,80 @@ public class homepage extends JFrame{
 
 	public void create(){
 		try{
-			url = new URL("https://i.imgur.com/62hTev9.gif");
+			url = new URL("http://hdwallpaper.freehdw.com/0004/3d-abstract_hdwallpaper_battleship_36265.jpg");
 		} catch (Exception e){
 			e.printStackTrace();
 		}
-		setLayout(new BorderLayout());
+
 		try{
-	    	Image image = new Image(new ImageIcon(ImageIO.read(url)));
+	    	setContentPane(new JLabel(new ImageIcon(ImageIO.read(url))));
 	   	} catch (Exception e){
 			e.printStackTrace();
 		}
 
-	    setLayout(new FlowLayout());
-	    l1=new JLabel("Here is a button");
-	    b1=new JButton("I am a button");
-	    add(l1);
-	    add(b1);
+		setLayout(new BorderLayout());
+
+	    b1 = new JButton("Player vs. Player");
+	    b2 = new JButton("Player vs. Computer");
+	    b3 = new JButton("Computer vs. Computer");
+	    b4 = new JButton("Rules of Battleship");
+
+	    panel.setPreferredSize(new Dimension(20,75));
+	    panel.setLayout(new GridLayout(4,1));
+	    panel.add(b1);
+	    panel.add(b2);
+	    panel.add(b3);
+	    panel.add(b4);
+
+	    title.setFont(font);
+		title.setForeground(new Color(10,150,250));
+
+	    add(panel, BorderLayout.SOUTH);
+	    add(title, BorderLayout.NORTH);
+
+	    // Player vs. Player
+	    b1.addActionListener(new ActionListener(){
+	    	public void actionPerformed(ActionEvent e){
+
+	    	}
+	    });
+
+	    // Player vs. Computer
+	   	b2.addActionListener(new ActionListener(){
+	    	public void actionPerformed(ActionEvent e){
+
+	    	}
+	    });
+
+	   	// Computer vs. Computer
+	    b3.addActionListener(new ActionListener(){
+	    	public void actionPerformed(ActionEvent e){
+
+	    	}
+	    });
+
+	    // Rules
+	    b4.addActionListener(new ActionListener(){
+	    	public void actionPerformed(ActionEvent e){
+	    		String message = "Players take turns firing a shot to attack enemy ships.\n" +
+	    		"On your turn, call out a letter and a number of a row and column on the \n" + 
+	    		"grid. Your opponent checks that space on their lower grid, and says \n" + 
+	    		"\"miss\" if there are no ships there, or boat are hit, the player loses\n" + 
+	    		"that boat. Once a player loses all thier boats, they have lost and the \n" + 
+	    		"other player has won.";
+	    		JOptionPane.showMessageDialog(null, message, "Rules of Battleship",
+	    			JOptionPane.INFORMATION_MESSAGE);
+	    	}
+	    });
+
 	    // Just for refresh :) Not optional!
-	    setSize(399,399);
-	    setSize(400,400);
+	    setSize(500,500);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setVisible(true);
-	    repaint();
+	    setResizable(false);
 	}
 
 	public void paintComponent(Graphics g){
-		add(pictue);
+		
 	}
 }
