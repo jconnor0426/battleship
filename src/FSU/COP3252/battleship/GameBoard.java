7953665c17224f607aac8f3093f3cefd97188bdd
Alexander Damiani <alexdmail15@yahoo.com>
2014-04-17 18:29:40 -0400

    Lots of update to gui and merged two gameboard files

--- src/FSU/COP3252/battleship/GameBoard.java
+++ src/FSU/COP3252/battleship/GameBoard.java
@@ -1,33 +1,55 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.util.Vector;
+import java.awt.GridLayout;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.event.*;
+import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Formatter;
-import java.util.Locale;
+import java.awt.Insets;
 
-public class GameBoard
-{
-        public static int players = 2;
+
+class gameboard extends JPanel{
+	public MyButton[][] button_array1 = new MyButton[10][10];
+	public MyButton[][] button_array2 = new MyButton[10][10];
+	public String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+	public JPanel panelEast = new JPanel(new BorderLayout());
+	public JPanel panelWest = new JPanel(new BorderLayout());
+	public JPanel mainPanel = new JPanel(new BorderLayout());
+	public static int players = 2;
 	public static int boardSize = 10;
-	private ArrayList< Ship > [] playerShips ;
-        private ArrayList<Integer> [] hits ;
-        private ArrayList<Integer> [] misses ;
+	private ArrayList< Ship > playerShips1 ;
+    private ArrayList<Integer> hits1 ;
+    private ArrayList<Integer> misses1 ;
+	private ArrayList< Ship > playerShips2 ;
+    private ArrayList<Integer> hits2 ;
+    private ArrayList<Integer> misses2 ;
 	
 
-	public GameBoard()
+	public gameboard()
 	{
-            for( int i = 0; i < players; i++ )
-            {
-		playerShips[i] = new ArrayList< Ship >();
-                hits[i] = new ArrayList<Integer>();
-                misses[i] = new ArrayList<Integer>();
-            }
+	    for( int i = 0; i < players; i++ ) {
+			playerShips1 = new ArrayList< Ship >(5);
+            hits1 = new ArrayList<Integer>(100);
+            misses1 = new ArrayList<Integer>(100);
+            playerShips2 = new ArrayList< Ship >(5);
+            hits2 = new ArrayList<Integer>(100);
+            misses2 = new ArrayList<Integer>(100);
+	    }
+	    createPanel();
 	}
 
-        public boolean placeShip( int team, Ship toPlace  )
-        {
-            if( validShipLocation( team, toPlace ) )
-                    playerShips[team].add(toPlace);
-            
-            return true;
-        }
+    /*public boolean placeShip( int team, Ship toPlace  )
+    {
+        if( validShipLocation( team, toPlace ) )
+                playerShips[team].add(toPlace);
+        
+        return true;
+    }
 
 	public boolean makeMove( int x, int y, char team)
 	{
@@ -45,5 +67,139 @@ public class GameBoard
     private boolean validShipLocation(int team, Ship toPlace) {
         
         return true;
-    }
+    }*/
+
+	// Sets up frame and adds panels
+	public void createPanel(){
+		panelEast.setLayout(new FlowLayout());
+		panelWest.setLayout(new FlowLayout());
+		setSize(500,500);
+		BoardOne board1 = new BoardOne();
+		BoardTwo board2 = new BoardTwo();
+		panelEast.add(board1);
+		panelWest.add(board2);
+		mainPanel.add(panelEast, BorderLayout.EAST);
+		mainPanel.add(panelWest, BorderLayout.WEST);
+		add(mainPanel);
+	}
+
+	// Stores button and x,y coordinates
+
+	// Left Board
+	class BoardOne extends JPanel implements ActionListener{
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			setPreferredSize(new Dimension(200,200));
+			GridLayout glayout = new GridLayout(11,10);
+			setLayout(glayout);
+
+			for (int i = 0; i < 11; i++){
+				if (i == 0){
+					JButton default_button = new JButton("");
+					default_button.setEnabled(false);
+					default_button.setMargin(new Insets(0, 0, 0, 0));
+					add(default_button);
+				}
+				else{
+					JButton letter_button = new JButton(letters[i-1]);
+					letter_button.setEnabled(false);
+					letter_button.setMargin(new Insets(0, 0, 0, 0));
+					add(letter_button);
+				}
+				for (int j = 0; j < 10; j++){
+					if (i > 0){
+						// New instance of MyButton
+						button_array1[i-1][j] = new MyButton(i-1, j);
+						button_array1[i-1][j].addActionListener(this);
+						add(button_array1[i-1][j]);
+					}
+					else{
+						String temp = Integer.toString(j);
+						JButton number_button = new JButton(temp);
+						number_button.setEnabled(false);
+						number_button.setMargin(new Insets(0, 0, 0, 0));
+						add(number_button);
+					}
+				}
+
+			}
+		}
+
+		public void actionPerformed(ActionEvent e){
+			MyButton button = (MyButton) e.getSource();
+			System.out.println("Board1 - " + button.getColumn() + ":" + letters[button.getRow()]);
+			button.setBackground(Color.BLACK);
+		}
+	}
+
+	// Right board
+	class BoardTwo extends JPanel implements ActionListener{
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			setPreferredSize(new Dimension(200,200));
+			GridLayout glayout = new GridLayout(11,11);
+			setLayout(glayout);
+
+			for (int i = 0; i < 11; i++){
+				if (i == 0){
+					JButton default_button = new JButton("");
+					default_button.setEnabled(false);
+					default_button.setMargin(new Insets(0, 0, 0, 0));
+					add(default_button);
+				}
+				else{
+					JButton letter_button = new JButton(letters[i-1]);
+					letter_button.setEnabled(false);
+					letter_button.setMargin(new Insets(0, 0, 0, 0));
+					add(letter_button);
+				}
+				for (int j = 0; j < 10; j++){
+					if (i > 0){
+						// New instance of MyButton
+						button_array2[i-1][j] = new MyButton(i-1, j);
+						button_array2[i-1][j].addActionListener(this);
+						add(button_array2[i-1][j]);
+					}
+					else{
+						String temp = Integer.toString(j);
+						JButton number_button = new JButton(temp);
+						number_button.setEnabled(false);
+						number_button.setMargin(new Insets(0, 0, 0, 0));
+						add(number_button);
+					}
+				}
+			}
+		}
+
+		public void actionPerformed(ActionEvent e){
+			MyButton button = (MyButton) e.getSource();
+			System.out.println("Board2 - " + button.getColumn() + ":" + letters[button.getRow()]);
+			button.setBackground(Color.BLACK);
+		}
+	}
+
+	public MyButton[][] getBoard1(){
+		return button_array1;
+	}
+
+	public MyButton[][] getBoard2(){
+		return button_array2;
+	}
+}
+
+class MyButton extends JButton{
+	private int row, column;
+
+	public MyButton(int x, int y){
+		column = x;
+		row = y;
+	}
+
+	public int getRow(){
+		return row;
+	}
+
+	public int getColumn(){
+		return column;
+	}
 }
---
+++ src/FSU/COP3252/battleship/MainPage.java
@@ -0,0 +1,138 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.util.Vector;
+import java.awt.GridLayout;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+import java.net.URL;
+import java.awt.Image;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.*;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.BorderFactory;
+import javax.swing.border.TitledBorder;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.util.Vector;
+import java.awt.GridLayout;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+import java.net.URL;
+import java.awt.Image;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.*;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.BorderFactory;
+import javax.swing.border.TitledBorder;
+
+class MainPage extends JFrame{
+	private JMenuBar menuBar;
+	private JMenu gameMenu, helpMenu;
+	private JMenuItem pvp, pvc, cvc, red, blue, green, yellow;
+
+	private String[] shipNames = {"carrier", "battleship", "submarine", "destroyer", "patrol boat"};
+	private String[] directionNames = {"Horizontal", "Vertical"};
+	private JButton deploy;
+	private JComboBox<String> shipsList = new JComboBox<String>(shipNames);
+	private JComboBox<String> directionsList = new JComboBox<String>(directionNames);
+	private TitledBorder ships, directions;
+	JPanel shipOptions;
+
+
+	public MainPage(){
+		addMenu();
+		optionMenus();
+	}
+
+	public void addMenu(){
+		menuBar = new JMenuBar();
+		gameMenu = new JMenu("Game");
+		JMenu newGame = new JMenu("New Game");
+		pvp = new JMenuItem("Player vs. Player");
+		pvc = new JMenuItem("Player vs. Computer");
+		cvc = new JMenuItem("Computer vs. Computer");
+
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				setVisible(false);
+				dispose();
+			}
+		});
+
+		helpMenu = new JMenu("Help");
+		JMenuItem help = new JMenuItem("View Help");
+		help.addActionListener(new HelpListener());
+
+		newGame.add(pvp);
+		newGame.add(pvc);
+		newGame.add(cvc);
+
+		gameMenu.add(newGame);
+		gameMenu.add(exit);
+
+		helpMenu.add(help);
+
+		menuBar.add(gameMenu);
+		menuBar.add(helpMenu);
+
+		setJMenuBar(menuBar);
+	}
+
+	public void optionMenus(){
+		deploy = new JButton("Place Ships!");
+		deploy.setEnabled(false);
+
+		ships = BorderFactory.createTitledBorder("Ships");
+		shipsList.setBorder(ships);	
+		directions = BorderFactory.createTitledBorder("Directions");
+		directionsList.setBorder(directions);
+
+		shipOptions = new JPanel();
+		shipOptions.setLayout(new FlowLayout());
+		shipOptions.add(shipsList);
+		shipOptions.add(directionsList);
+		shipOptions.add(deploy);
+
+		add(shipOptions, BorderLayout.NORTH);
+	}
+
+	private class HelpListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			String message = "Players take turns firing a shot to attack enemy ships.\n" +
+    		"On your turn, call out a letter and a number of a row and column on the \n" + 
+    		"grid. Your opponent checks that space on their lower grid, and says \n" + 
+    		"\"miss\" if there are no ships there, or boat are hit, the player loses\n" + 
+    		"that boat. Once a player loses all thier boats, they have lost and the \n" + 
+    		"other player has won.";
+    		JOptionPane.showMessageDialog(null, message, "Rules of Battleship",
+    		JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+}
