49f39940cdfd9eee17de1d2685c824a5c076e254
Connor <Connor@192.168.72.54>
2014-04-19 14:19:53 -0400

    First Version of placing ships to the game object

--- src/FSU/COP3252/battleship/GameObject.java
+++ src/FSU/COP3252/battleship/GameObject.java
@@ -13,30 +13,69 @@ import java.util.ArrayList;
  */
 public class GameObject {
     
+    public static int HORIZONTAL = 0;
+    public static int VERTICAL = 1;
+    
+    public static int GAMESIZE = 10;
+    
+    
     MyButton [] [][] game;
     ArrayList< Ship > [] playerShips;
+    ArrayList< Integer > [] hits;
+    ArrayList< Integer > [] misses;
     
     public GameObject( MyButton[][] team0Buttons, MyButton[][] team1Buttons )
     {
         game[0] = team0Buttons;
         game[1] = team1Buttons;
+        
+        playerShips[0] = new ArrayList< Ship >(0);
+        playerShips[1] = new ArrayList< Ship >(0);
     }
     
     
     boolean placeShip( int x, int y, int orientation, Ship toPlace, int team )
     {
+        //Check to see if the ship is already on the board
+        
+        
+        toPlace.setRow( x );
+        toPlace.setColumn( y );
+        toPlace.setOrientation( orientation );
         
         //Try to create ship 
+        if( orientation == HORIZONTAL )
+        {
+            if( ! validHorizontal(x, toPlace.getSize() ) )
+                return false;
+        }else if( orientation == VERTICAL )
+        {
+            if( ! validVertical( y, toPlace.getSize() ) )
+                return false;
+        }
         
         //Try to place 
-        
-        //If bad placement return false
+        if( ! checkValid(  toPlace, team ) )
+            return false;
         
         //If good placement add ship to team array
+        playerShips[team].add(toPlace );
+        
         //return true
         
         return true;
     }
+    
+    boolean validHorizontal( int x, int size )
+    {
+        return x + size > GAMESIZE;
+    }
+    
+    boolean validVertical( int y, int size )
+    {
+        return y + size > GAMESIZE;
+    }
+    
     
     ArrayList< Ship > getShipsToDraw( int team )
     {
@@ -46,14 +85,34 @@ public class GameObject {
     boolean spotOccupied( int x, int y, int team )
     {
         //Return whether or not a ship occupies that location
-        //return game[ team ][x][y].getOccupied();
-        return true;
+        return game[ team ][x][y].getOccupied();        
     }
     
     boolean spotHit( int x, int y, int team )
     {
         //Return whether or not a ship occupies that location
         //return game[ team ][x][y].getOccupied();
+        return true;
+    }
+    
+    boolean checkValid(  Ship toCheck , int team  )
+    {
+        if( toCheck.getOrientation() == HORIZONTAL )
+        {
+            for( int i = 0; i < toCheck.getSize(); i++ )
+            {
+                if( spotOccupied( toCheck.getRow(), toCheck.getColumn() + i, team ) )
+                    return false;
+            }
+        }else
+        {
+           for( int i = 0; i < toCheck.getSize(); i++ )
+            {
+                if( spotOccupied( toCheck.getRow() + i, toCheck.getColumn(), team ) )
+                    return false;
+            } 
+        }
+        
         return true;
     }
     
