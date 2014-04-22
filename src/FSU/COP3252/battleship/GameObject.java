29edf35546f225dd5607febb004eb79b9037abff
Connor <Connor@10.188.197.21>
2014-04-22 19:14:10 -0400

    Medium and Hard seem to be working

--- src/FSU/COP3252/battleship/GameObject.java
+++ src/FSU/COP3252/battleship/GameObject.java
@@ -321,16 +321,7 @@ class HumanVsComputerEasy extends GameObject
 
 class HumanVsComputerMedium extends GameObject
 {
-    protected class Couple{
-        public int x;
-        public int y;
-        public Couple( int x, int y )
-        {
-            this.x = x;
-            this.y = y;
-        }
-        
-    }
+    
     
     protected Stack< Couple > computerTarget;
 
@@ -350,7 +341,7 @@ class HumanVsComputerMedium extends GameObject
             //If Game is not over, CPU takes a turn
             if( checkGameOver() == -1 )
             {
-               cpuTurn( teamToAttack, team );
+               cpuTurn( cpuGetTarget(), teamToAttack, team );
             }
             //CPU decision: basic mode will be just random choice
         }
@@ -380,9 +371,8 @@ class HumanVsComputerMedium extends GameObject
         return target;
     }
     
-    private boolean cpuTurn(int team, int teamToAttack)
+    private boolean cpuTurn(Couple target, int team, int teamToAttack)
     {
-        Couple target = cpuGetTarget();
         
         while( ! super.takeTurn(target.x, target.y, team, teamToAttack ) )
         {
@@ -413,22 +403,29 @@ class HumanVsComputerMedium extends GameObject
     
 }
 
-class HumanVsComputerHard extends HumanVsComputerMedium
+class HumanVsComputerHard extends GameObject
 {
 
+    protected Stack< Couple > computerTarget;
+    
     public HumanVsComputerHard(MyButton[][] team0Buttons, MyButton[][] team1Buttons) {
         super(team0Buttons, team1Buttons);
+        cpuInitialize(1);
     }
 
     private Couple cpuGetTarget()
     {
-        System.out.println( "DEBUGGING! YOU UNDERSTAND INHERITANCE!");
+        
         Couple target;
         Random gen = new Random();
         //If stack is empty
         if( computerTarget.empty())
         {
-            target = new HumanVsComputerMedium.Couple( gen.nextInt(5)*2, gen.nextInt(5)*2 );
+            //Select from only half the board
+            int x = gen.nextInt(10);
+            int y = (x%2==0)?   gen.nextInt(5)*2+1 //if even, select odd
+                            :   gen.nextInt(5)*2;  //else odd, select even
+            target = new Couple( x, y );
         }else
         {
             target = computerTarget.pop();
@@ -437,4 +434,67 @@ class HumanVsComputerHard extends HumanVsComputerMedium
         return target;
     }
     
+    public boolean takeTurn(int x, int y, int team, int teamToAttack )
+    {
+        boolean userResult = super.takeTurn(x, y, team, teamToAttack);
+        if( userResult ) //If the user made a valid move
+        {
+            //If Game is not over, CPU takes a turn
+            if( checkGameOver() == -1 )
+            {
+               cpuTurn( cpuGetTarget(), teamToAttack, team );
+            }
+            //CPU decision: basic mode will be just random choice
+        }
+        return userResult;
+    }
+    
+    private boolean cpuTurn(Couple target, int team, int teamToAttack)
+    {
+        
+        while( ! super.takeTurn(target.x, target.y, team, teamToAttack ) )
+        {
+            target = cpuGetTarget();
+        };
+        
+        //If shot hit, add  N, S, E, W, to stack
+        if( spotHit( target.x, target.y, teamToAttack) && spotOccupied(target.x, target.y, teamToAttack) )
+        {
+            //Add x+1, y
+            if( target.x +1 < 10 )
+                computerTarget.push( new Couple( target.x +1 , target.y ) );
+            //Add x-1, y
+            if( target.x -1 >= 0 )
+                computerTarget.push( new Couple( target.x -1 , target.y ) );
+            //Add x, y+1
+            if( target.y +1 < 10 )
+                computerTarget.push( new Couple( target.x  , target.y+1 ) );
+            //Add x, y-1
+            if( target.y - 1 >= 0 )
+                computerTarget.push( new Couple( target.x , target.y -1 ) );
+        }
+        
+        
+        
+        return true;
+    }
+    
+    public void cpuInitialize( int team )
+    {
+        super.cpuInitialize(team);
+        
+        computerTarget = new Stack< Couple >();
+    }
+    
 }
+
+class Couple{
+        public int x;
+        public int y;
+        public Couple( int x, int y )
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+    }
